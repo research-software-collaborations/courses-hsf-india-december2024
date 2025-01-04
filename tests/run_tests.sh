b48b15fe472a5601039454d93fba1723f5ea3285
David Lange <David.Lange@cern.ch>
2025-01-04 15:54:00 +0100

    add tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,16 +5,23 @@ nvcc -o hello hello.cu
 echo "building vadd"
 nvcc -o vadd vadd.cu
 
-#not part of the notebook
-#echo "building cudnn_test"
-#g++ -c cudnn_test.cc -o cudnn_test.o -I/srv/conda/envs/notebook/lib/python3.11/site-packages/nvidia/cuda_runtime/include/ -I/srv/conda/envs/notebook/targets/x86_64-linux/include/ -I/srv/conda/envs/notebook/lib/python3.11/site-packages/nvidia/cudnn/include/
-#nvcc -ccbin g++ -m64 -o cudnn_test cudnn_test.o -lcublasLt -lcudart -lcublas -lcudnn -lstdc++ -lm -L/srv/conda/envs/notebook/lib
-
 echo "running hello"
 ./hello
 echo "running vadd"
 ./vadd
-#echo "running cudnn_test"
-#./cudnn_test
+
+echo "running cudnn_test"
+python train_cudnn.py
+
+echo "running pytorch"
+python pytorch_test.py
+
+echo "running jax"
+chmod +x jax_test.sh
+./jax_test.sh
+
+echo "testing nsys"
+nsys profile -t nvtx,cuda -o nsys_out.txt --stats=true --force-overwrite true ./hello
 
 echo "done"
+
